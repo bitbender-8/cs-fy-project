3405abeff2ee44d29edb1e17d38c3e7e698e4e76
bitbender-8 <111208298+bitbender-8@users.noreply.github.com>
2024-11-30 01:02:22 +0300

    Testing changes to express-ci-tasks

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,7 @@ import express, { Application, Request, Response } from "express";
 const app: Application = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Respo nse) => {
   res.send("Express + TypeScript Server");
 });
 
