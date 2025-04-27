3f2c23a31b2f0c3a8eda2949af1d769232b276a7
Haileyesus Dessie <118998054+blackmammoth@users.noreply.github.com>
2025-04-28 01:10:16 +0300

    feat: Connect donation chart to database

--- frontends/admin/src/app/page.tsx
+++ frontends/admin/src/app/page.tsx
@@ -112,6 +112,35 @@ export default async function BroswseCampaignsHomePage() {
     },
   ];
 
+  // get the chart data for the last 3 months
+  const rawDonations = await prisma.campaignDonation.findMany({
+    where: {
+      createdAt: {
+        gte: new Date(Date.now() - 90 * 24 * 60 * 60 * 1000), // donations in the last 3 months
+      },
+    },
+    select: {
+      createdAt: true,
+      grossAmount: true,
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+  });
+
+  // convert the raw donations to a map with the date as key and the amount as value
+  // and sum the amounts for each date
+  const mapDonations = new Map<string, number>();
+  for (const { createdAt, grossAmount } of rawDonations) {
+    const day = createdAt.toISOString().slice(0, 10); // "2025-04-23"
+    mapDonations.set(day, (mapDonations.get(day) ?? 0) + Number(grossAmount));
+  }
+
+  // convert the map to an array of objects with date and amount properties
+  const aggregatedDonations = Array.from(mapDonations.entries())
+    .map(([date, amount]) => ({ date, amount }))
+    .sort((a, b) => a.date.localeCompare(b.date));
+
   return (
     <div>
       <div className="flex justify-between items-center">
@@ -132,7 +161,7 @@ export default async function BroswseCampaignsHomePage() {
       </div>
 
       <div className="mt-5">
-        <DashboardGraph />
+        <DashboardGraph initialData={aggregatedDonations} />
       </div>
     </div>
   );
