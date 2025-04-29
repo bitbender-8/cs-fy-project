e39b6f9d7183cdded5c48044fc1fe2f766b0e03a
Haileyesus Dessie <118998054+blackmammoth@users.noreply.github.com>
2025-04-29 22:34:06 +0300

    feat: Add roles to authentication
    Only supervisors have access to the system.

--- frontends/admin/src/middleware.ts
+++ frontends/admin/src/middleware.ts
@@ -1,4 +1,5 @@
 import { NextResponse, type NextRequest } from "next/server";
+import { jwtDecode } from "jwt-decode";
 
 import { auth0 } from "./lib/auth0";
 
@@ -15,6 +16,19 @@ export async function middleware(request: NextRequest) {
     // user is not authenticated, redirect to login page
     return NextResponse.redirect(
       new URL("/auth/login", request.nextUrl.origin)
+    );
+  }
+
+  // This is it, check your console.log()
+  const roleKey = `${process.env.AUTH0_NAMESPACE}/roles`;
+  const accessToken = await auth0.getAccessToken();
+  const decodedAccessToken = jwtDecode<{ [key: string]: string[] }>(
+    accessToken.token
+  );
+  const role = decodedAccessToken[roleKey];
+  if (role[0] !== "Supervisor") {
+    return NextResponse.redirect(
+      new URL("/auth/logout", request.nextUrl.origin)
     );
   }
 
