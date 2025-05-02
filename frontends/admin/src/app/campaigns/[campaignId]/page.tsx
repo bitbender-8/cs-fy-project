b532e89a1367e8a87315da05b58e3ff0b8ccdad1
Haileyesus Dessie <118998054+blackmammoth@users.noreply.github.com>
2025-05-02 23:12:14 +0300

    feat: Connect campaign overview with database

--- frontends/admin/src/app/campaigns/[campaignId]/page.tsx
+++ frontends/admin/src/app/campaigns/[campaignId]/page.tsx
@@ -5,45 +5,92 @@ import { DialogDecline } from "@/components/dialog-decline";
 import { Button } from "@/components/ui/button";
 import { Progress } from "@/components/ui/progress";
 import { Textarea } from "@/components/ui/textarea";
+import { Campaign, CampaignDonation, Prisma } from "@/generated/prisma";
+import prisma from "@/lib/prisma";
 import { ArrowBigRight } from "lucide-react";
 
 type CampaignPageProps = {
   params: Promise<{
-    id: string;
+    campaignId: string;
   }>;
 };
 
-export default async function CampaignPage({ params }: CampaignPageProps) {
-  const { id } = await params;
+type CampaignWithDonations = Prisma.CampaignGetPayload<{
+  include: {
+    CampaignDonation: true;
+    CampaignDocuments: true;
+    CampaignPost: true;
+  };
+}>;
 
-  // TODO: To be deleted later
-  console.log("Campaign Id: ", id);
+export default async function CampaignPage({ params }: CampaignPageProps) {
+  const { campaignId } = await params;
+
+  let campaign: CampaignWithDonations | null;
+
+  try {
+    campaign = await prisma.campaign.findUnique({
+      where: { id: campaignId },
+      include: {
+        CampaignDonation: true,
+        CampaignDocuments: true,
+        CampaignPost: true,
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching campaign:", error);
+    return <p>Error fetching campaign data</p>;
+  }
+
+  if (!campaign) {
+    return <p>Campaign not found</p>;
+  }
+
+  const {
+    title,
+    submissionDate,
+    fundraisingGoal,
+    endDate,
+    CampaignDonation,
+    status,
+  } = campaign;
+
+  console.log("Status: ", status)
+  let amountRaised = 0;
+  CampaignDonation.forEach((donation) => {
+    amountRaised += Number(donation.grossAmount);
+  });
+
+  const remaining = Number(fundraisingGoal) - amountRaised;
+  const remainingAmount = remaining > 0 ? remaining : 0;
+
+  const progressValue = (amountRaised / Number(fundraisingGoal)) * 100;
+
   return (
     <div className="flex flex-col gap-5">
       <div className="flex gap-3 justify-between">
         <h1 className="text-primary">Campaign Overview</h1>
-        <ChangeCampaignVisibilityDropdownMenu />
+        <ChangeCampaignVisibilityDropdownMenu status={status} campaignId={campaignId} />
       </div>
 
       <div className="flex justify-between">
         <div className="space-y-2 bg-gray-200 p-5">
           <p className="text-xl font-semibold">
-            <span className="text-primary mr-2">Campaign Title: </span>Kaleb
-            Kidney Transplant
+            <span className="text-primary mr-2">Campaign Title: </span> {title}
           </p>
           <p className="text-xl font-semibold">
             <span className="text-primary mr-2">
               Campaign Submission Date:{" "}
             </span>
-            Jan. 5, 2024
+            {new Date(submissionDate).toLocaleDateString()}
           </p>
           <p className="text-xl font-semibold">
             <span className="text-primary mr-2">Fundraising Goal: </span>
-            2,500,000 ETB
+            {fundraisingGoal.toLocaleString()} ETB
           </p>
           <p className="text-xl font-semibold">
-            <span className="text-primary mr-2">Campaign End Date: </span>Mar.
-            29, 2024
+            <span className="text-primary mr-2">Campaign End Date: </span>
+            {endDate && new Date(endDate).toLocaleDateString()}
           </p>
         </div>
 
@@ -52,10 +99,16 @@ export default async function CampaignPage({ params }: CampaignPageProps) {
             <h3 className="text-primary text-xl font-semibold">
               Amount Raised
             </h3>
-            <p>400,000 / 2,500,000 ETB</p>
+            <p>
+              {" "}
+              {amountRaised.toLocaleString()} /{" "}
+              {fundraisingGoal.toLocaleString()} ETB
+            </p>
           </div>
-          <Progress value={33} className="h-5" />
-          <p className="text-right text-xs">Remaining: 1,500,000 ETB</p>
+          <Progress value={progressValue} className="h-5" />
+          <p className="text-right text-xs">
+            Remaining: {remainingAmount.toLocaleString()} ETB
+          </p>
         </div>
       </div>
 
