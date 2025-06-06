a043c292c77268c802a92123ec4d2677715283c6
Haileyesus Dessie <118998054+blackmammoth@users.noreply.github.com>
2025-06-06 16:10:56 +0300

    fix: Remove POST method from form submission as next js handles it

--- frontends/admin/src/app/campaigns/[campaignId]/page.tsx
+++ frontends/admin/src/app/campaigns/[campaignId]/page.tsx
@@ -300,7 +300,6 @@ export default async function CampaignPage({ params }: CampaignPageProps) {
 
       <form
         action={submitCampaignUpdate}
-        method="POST"
         className="flex flex-col gap-3"
       >
         <h2 className="text-primary">Update campaign</h2>
